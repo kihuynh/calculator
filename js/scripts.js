41f641819f1693e6b9410956d6540e5d09bb38e4
Jared Clemmensen and Kimberly Huynh <jared@>
2018-01-10 11:01:23 -0800

    add other button functions

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,32 @@ var divide = function (number1, number2){
 
 $(document).ready(function() {
 
-	$("form#add").submit(function(event) {
-	event.preventDefault();
-	var number1 = parseInt($("#first").val());
-	var number2 = parseInt($("#second").val());
-	var result = add(number1, number2);
-	$("#output").text(result);
-});
+	$("button#add").click(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#first").val());
+		var number2 = parseInt($("#second").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+	$("button#subtract").click(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#first").val());
+		var number2 = parseInt($("#second").val());
+		var result = subtract(number1, number2);
+		$("#output").text(result);
+	});
+	$("button#multiply").click(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#first").val());
+		var number2 = parseInt($("#second").val());
+		var result = multiply(number1, number2);
+		$("#output").text(result);
+	});
+	$("button#divide").click(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#first").val());
+		var number2 = parseInt($("#second").val());
+		var result = divide(number1, number2);
+		$("#output").text(result);
+	});
 });
